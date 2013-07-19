1ea118d118074f6649b3eaea6d4b14f6948bd941
Jarek Sedlacek <jareksedlacek@gmail.com>
2013-07-18 20:09:15 -0400

    Version 1.0.1 Beta 1
    - Changed to use the real chrome regexes for card # and cardholder name
    - Just gives 3 javascript alerts when it detects a checkout attempt
      (both regexes hit)
    - After some testing, we'll see how to make it target pages better
    - Still needs some better popup or warning message.

--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,26 @@
 // Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//var regex = /number|card.?#|card.?no|ccnum/gi;
-var regex = /number/gi;
-matches = document.body.innerHTML.match(regex);
-if (matches) {
-  console.log("looks like we found 'number'");
-  var payload = {
-    count: matches.count()
-  };
-  chrome.extension.sendRequest(payload, function(response) {});
-} else {
-    console.log("it looks like 'number' is not in the html for the body...");
+var regexes = { "number" : /number|card.?#|card.?no|ccnum/gi, "name" : /card.?holder|name.?on.?card|ccname|owner/gi}
+var found_all = true;
+for( var regex in regexes) {
+    matches = document.body.innerText.match(regexes[regex]);
+    if (!matches){
+        console.log("couldn't find the regex for " + regex);
+        found_all = false;
+        break;
+    }
+    console.log("Found " + regex);
 }
+
+if(found_all){
+    console.log("looks like we found everything");
+    alert("Looks like you are buying things");
+    alert("Are you sure you want to buy things?");
+    alert("Buying things costs money!");
+    var payload = {
+        count: matches.count()
+    };
+    chrome.extension.sendRequest(payload, function(response) {});
+}
+
